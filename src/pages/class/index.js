3075ce26e538e6f6e1fd625a803e6b2bc42caa52
jsHunter <470759230@qq.com>
2021-06-21 14:56:08 +0800

    ref practice

--- src/pages/class/index.js
+++ src/pages/class/index.js
@@ -51,6 +51,11 @@ export default class Index extends Component {
               modal
             </Link> 
           </List.Item>
+          <List.Item>
+            <Link to="/class/refs">
+              refs
+            </Link> 
+          </List.Item>
         </List>
       </div>
     )
